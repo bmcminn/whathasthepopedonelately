26ba0c783ffaac4311087ef209c4c628a1a37bc9
bmcminn <labs@gbox.name>
2015-01-05 18:01:39 -0600

    adding updated files...

---
+++ app/js/app.js
@@ -0,0 +1,17 @@
+require('angular/angular');
+require('angular-route/angular-route');
+
+angular.module('popeApp', ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'partials/parital1.html'
+    });
+
+    $routeProvider.when('/view2', {
+      templateUrl: 'partials/parital2.html'
+    });
+
+    $routeProvider.otherwise({
+      redirectTo: '/view1'
+    });
+  }]);
--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,48 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  })
-}
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        stylus: {
+            app: {
+                files: {
+                    './app/css/theme.css': './app/stylus/theme.styl'
+                }
+            }
+        },
+
+        jshint: {
+            all: [
+                './gruntfile.js',
+                './app/js/**/*.js'
+            ]
+        },
+
+        browserify: {
+            './app/dest/app.js': ['./client/app.js']
+        },
+
+        watch: {
+            js: {
+                files: [
+                    './gruntfile.js',
+                    './app/js/**/*.js'
+                ],
+                tasks: ['jshint', 'browserify']
+            },
+            css: {
+                files: [
+                    './app/stylus/**/*.styl'
+                ],
+                tasks: ['stylus']
+            }
+        }
+
+    });
+
+
+    grunt.registerTask('default', ['stylus', 'jshint', 'browserify']);
+
+};
