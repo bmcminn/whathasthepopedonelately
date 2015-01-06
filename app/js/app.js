a3a9e9dda36525e36af769bcf27aa6bbe7ce4ebc
bmcminn <labs@gbox.name>
2015-01-06 17:17:28 -0600

    gruntfile and builds are working

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,24 @@
+'use strict';
+
 require('angular');
 require('angular-route');
 
-angular.module('popeApp', ['ngRoute'])
+
+var app = angular.module('popeApp', ['ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {
-      templateUrl: 'partials/parital1.html'
+      templateUrl: '/partials/partial1.html'
     });
 
     $routeProvider.when('/view2', {
-      templateUrl: 'partials/parital2.html'
+      templateUrl: '/partials/partial2.html'
     });
 
     $routeProvider.otherwise({
       redirectTo: '/view1'
     });
   }]);
+
+
+app
+  .controller('homeCtrl', [])
--- gruntfile.js
+++ gruntfile.js
@@ -1,48 +1,74 @@
+
+'use strict';
+
 module.exports = function(grunt) {
 
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        stylus: {
-            app: {
-                files: {
-                    './app/css/theme.css': './app/stylus/theme.styl'
-                }
-            }
-        },
-
-        jshint: {
-            all: [
-                './gruntfile.js',
-                './app/js/**/*.js'
-            ]
-        },
-
-        browserify: {
-            './app/build/app.js': ['./client/app.js']
-        },
-
-        watch: {
-            js: {
-                files: [
-                    './gruntfile.js',
-                    './app/js/**/*.js'
-                ],
-                tasks: ['jshint', 'browserify']
-            },
-            css: {
-                files: [
-                    './app/stylus/**/*.styl'
-                ],
-                tasks: ['stylus']
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    path: {
+      gruntfile: './gruntfile.js',
+      prod: './app/_prod/',
+      build: './app/_build/',
+      js: './app/js/',
+      css: './app/css/',
+      stylus: './app/stylus/'
+    },
+    stylus: {
+      app: {
+        files: {
+          '<%= path.css %>theme.css': '<%= path.stylus %>theme.styl'
         }
+      }
+    },
 
-    });
+    jshint: {
+      all: [
+        '<%= path.gruntfile %>',
+        '<%= path.js %>**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      }
+    },
 
+    browserify: {
+      build: {
+        src: '<%= path.js %>app.js',
+        dest: '<%= path.build %>app.js',
+      }
+    },
 
-    grunt.registerTask('default', ['stylus', 'jshint', 'browserify']);
+    uglify: {
+      options: {
+        mangle: true
+      },
+      prod: {
+        files: {
+          '<%= path.prod %>/app.js': '<%= path.build %>/app.js'
+        }
+      }
+    },
+
+    watch: {
+      js: {
+        files: [
+          '<%= path.gruntfile %>',
+          '<%= path.js %>**/*.js'
+        ],
+        tasks: ['jshint', 'browserify', 'uglify']
+      },
+      css: {
+        files: [
+          '<%= path.stylus %>**/*.styl'
+        ],
+        tasks: ['stylus']
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['stylus', 'jshint', 'browserify', 'uglify']);
 
 };
